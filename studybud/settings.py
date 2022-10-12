ec099548414fef727572eb62ae95318039894087
AdarshVMore <skullcrushermore@gmail.com>
2022-10-12 20:03:33 +0530

    50% of stylinig done

--- studybud/settings.py
+++ studybud/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/style/'
 
 MEDIA_URL = '/image/'
 
